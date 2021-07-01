ce6f43b3955435ee394c9acab5423e5f95890390
IVaN-KaLNYTSKYI <ivan.kalnytskyi@gmail.com>
2021-07-01 18:07:57 +0300

    hm-5

--- hm-5/helpers/password-hasher.js
+++ hm-5/helpers/password-hasher.js
@@ -4,9 +4,9 @@ const { ErrorHandler } = require('../errors');
 module.exports = {
     hash: (password) => bcrypt.hash(password, 10),
     compare: async (password, hashPassword) => {
-        const isPasswordEquals = await bcrypt.compare(password, hashPassword);
+        const isPassword = await bcrypt.compare(password, hashPassword);
 
-        if (!isPasswordEquals) {
+        if (!isPassword) {
             throw new ErrorHandler(400, 'RERE', 'EEW');
         }
     }
